5f4730e33fccebe3746afaee2bfb3cc904feb0b1
0xbagholder <101540063+0xbagholder@users.noreply.github.com>
2022-06-19 15:59:29 +0800

    Update 01-deploy-sample.js

--- deploy/01-deploy-sample.js
+++ deploy/01-deploy-sample.js
@@ -2,6 +2,7 @@
 module.exports = async ({deployments, getNamedAccounts}) => {
     const {deploy, log} = deployments;
     const {deployer} = await getNamedAccounts();
+    log("====================================================================");
 }
 
 module.exports.tags =['all']
